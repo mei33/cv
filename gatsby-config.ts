213639c7b447f2a603c760e5fc94756ad864f633
Maksim Andreev <ma.andreev@corp.vk.com>
2022-06-21 22:18:13 +0300

    Add svg support

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,16 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images\/icons\/.*\.svg/,
+        },
+      },
+    },
+  ],
 };
 
 export default config;
