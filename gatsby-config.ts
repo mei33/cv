3fd857c8d89f9a31218fa4d024eb11c6efe7d9f8
Maksim Andreev <ma.andreev@corp.vk.com>
2022-06-21 22:16:59 +0300

    Update gatsby site metadata config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `cv`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Maksim Andreev CV`,
+    siteUrl: `https://mei33.github.io/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
